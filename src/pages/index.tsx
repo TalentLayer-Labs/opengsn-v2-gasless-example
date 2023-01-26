8d4aa60b76deb7e390719647f1cc61fca12a8190
mattiapomelli <mattiapomelli@gmail.com>
2023-01-26 12:26:09 +0000

    Add loading spinner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,7 @@ const Home = () => {
 
   const [profile, setProfile] = useState<Profile | null>();
   const [handle, setHandle] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const [talentLayerID, setTalentLayerID] = useState<ethers.Contract | null>(
     null
@@ -88,10 +89,12 @@ const Home = () => {
 
   const onMint = async () => {
     if (!talentLayerID) return;
+    setLoading(true);
 
     const tx = await talentLayerID.mint(platformId, handle);
     await tx.wait();
 
+    setLoading(false);
     getProfile();
   };
 
@@ -125,8 +128,28 @@ const Home = () => {
                 value={handle}
                 onChange={(e) => setHandle(e.target.value)}
               />
-              <button className={styles.button} onClick={onMint}>
+              <button
+                disabled={loading}
+                className={styles.button}
+                onClick={onMint}
+              >
+                {/* Spinner */}
                 Mint
+                {loading && (
+                  <svg
+                    width="1rem"
+                    height="1rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className={styles.spinner}
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    />
+                  </svg>
+                )}
               </button>
             </div>
           </div>
